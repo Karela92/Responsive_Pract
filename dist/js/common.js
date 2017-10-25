6716b36f478525a39f0160f6dc89581ba9d7962b
Dima Karelin <karelinds@mail.ru>
2017-10-25 15:27:20 +0400

    Responsive

---
+++ dist/js/common.js
@@ -0,0 +1,31 @@
+$(function(){
+
+	// Яндекс метрики и гугл Аналитика
+
+	$('.count_element').on('click',function(){
+		ga('send','event','goal','goal');
+		yaCounterXXXXXXXX.reachGoal('goal');
+		return true;
+	});
+
+
+	$('.say__wrap').owlCarousel({
+    loop: true,
+    nav: true,
+    navText : ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
+    responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+//$('body').fadeOut();
+
+});
